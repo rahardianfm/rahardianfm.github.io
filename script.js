8c6ae90c108a3b26d67b977082499b02c28c668c
Rahardian Fakhri <rahardianfm@gmail.com>
2021-12-12 22:38:42 +0700

    update project page

---
+++ script.js
@@ -0,0 +1,8 @@
+// const thumbs = document.getElementsByClassName('thumb')[0];
+// const icons = thumbs.querySelectorAll('img');
+
+// thumbs.addEventListener('click', function(e){
+//     if (e.target.contains('img')){
+//         alert('ok');
+//     }
+// });
